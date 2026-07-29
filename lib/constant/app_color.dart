516ed95c1e2f1316ac3f5f4989bfc6eacd083f00
Zedts <savana.cell152@gmail.com>
2026-07-29 13:24:57 +0700

    Day 11 Navigator & Routing

---
+++ lib/constant/app_color.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/widgets.dart';
+
+class AppColor {
+  static const Color black22 = Color(0xff222222);
+  static const Color gray88 = Color(0xff888888);
+  static const Color blueButton = Color(0xff283FB1);
+  static const Color orange = Color(0xffF34B1B);
+  static const Color army1 = Color(0xff73946B);
+  static const Color army2 = Color(0xffD2D0A0);
+  static const Color army3 = Color(0xff9EBC8A);
+  static const Color secondaryColor = Color(0xff9EBC8A);
+  static const Color redColor = Color(0xffF34B1B);
+  static const Color primaryColor = Color(0xff73946B);
+
+  //Warna setetes
+
+  static const Color blackText = Color(0xff1D0E0C);
+  static const Color greyText = Color(0xff6B7280);
+
+  static const Color borderColor = Color(0xffE5E7EB);
+  static const Color redSetetesColor = Color(0xffD61000);
+  static const Color greyText2 = Color(0xff4B5563);
+
+  //gradient color
+  static const Color gradientColor1 = Color(0xff4983F6);
+  static const Color gradientColor2 = Color(0xffC175F5);
+  static const Color gradientColor3 = Color(0xffFBACB7);
+  static const Color greyColorOr = Color(0xff6C7278);
+  static const Color greyColorDivider = Color(0xffEDF1F3);
+  static const Color blueForgot = Color(0xff4D81E7);
+  static const Color blueButtons = Color(0xff1D61E7);
+}
