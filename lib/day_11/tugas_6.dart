a0c668580162299ac14801113babf251bf5a6ba3
Zedts <savana.cell152@gmail.com>
2026-07-29 14:13:23 +0700

    Tugas 6 Futter Frame 15

---
+++ lib/day_11/tugas_6.dart
@@ -0,0 +1,289 @@
+import 'package:flutter/material.dart';
+
+class Tugas6 extends StatefulWidget {
+  const Tugas6({super.key});
+
+  @override
+  State<Tugas6> createState() => _Tugas6State();
+}
+
+class _Tugas6State extends State<Tugas6> {
+  bool obscurePassword = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 34),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              SizedBox(height: 24),
+
+              // Logo
+              Center(
+                child: Column(
+                  children: [
+                    Image.asset(
+                      'assets/logo/smile.png',
+                      width: 150,
+                      height: 150,
+                      fit: BoxFit.contain,
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      'Welcome back!',
+                      style: TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              SizedBox(height: 24),
+
+              // Email
+              _buildTextField(
+                hintText: 'Email',
+                prefixIcon: Icons.mail_outline,
+              ),
+
+              SizedBox(height: 16),
+
+              // Password
+              _buildTextField(
+                hintText: 'Password',
+                prefixIcon: Icons.lock_outline,
+                obscureText: obscurePassword,
+                suffixIcon: IconButton(
+                  onPressed: () {
+                    setState(() {
+                      obscurePassword = !obscurePassword;
+                    });
+                  },
+                  icon: Icon(
+                    obscurePassword ? Icons.visibility_off : Icons.visibility,
+                    color: Colors.grey,
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 8),
+
+              Align(
+                alignment: Alignment.centerRight,
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Forgot password?',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 8),
+
+              // Login button
+              SizedBox(
+                height: 56,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => NextPage(),
+                      ),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFF1096F4),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    elevation: 0,
+                  ),
+                  child: Text(
+                    'Login',
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.w700,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 16),
+
+              // Garis "or"
+              Row(
+                children: [
+                  Expanded(
+                    child: Divider(
+                      thickness: 1,
+                      color: Color(0xFFD0D0D0),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    child: Text(
+                      'or',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.grey.shade700,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Divider(
+                      thickness: 1,
+                      color: Color(0xFFD0D0D0),
+                    ),
+                  ),
+                ],
+              ),
+
+              SizedBox(height: 16),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  _SocialAssetButton(
+                    assetPath: 'assets/images/icons/Facebook.png',
+                  ),
+                  SizedBox(width: 18),
+                  _SocialAssetButton(
+                    assetPath: 'assets/images/icons/Google.png',
+                  ),
+                  SizedBox(width: 18),
+                  _SocialAssetButton(
+                    assetPath: 'assets/images/icons/Apple.png',
+                  ),
+                ],
+              ),
+
+              Spacer(),
+
+              Center(
+                child: RichText(
+                  text: TextSpan(
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black,
+                    ),
+                    children: [
+                      TextSpan(text: "Don't have an account? "),
+                      TextSpan(
+                        text: 'Sign up',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              SizedBox(height: 16),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  // Reusable TextField
+  Widget _buildTextField({
+    required String hintText,
+    required IconData prefixIcon,
+    bool obscureText = false,
+    Widget? suffixIcon,
+  }) {
+    return TextField(
+      obscureText: obscureText,
+      style: TextStyle(fontSize: 16),
+      decoration: InputDecoration(
+        hintText: hintText,
+        hintStyle: TextStyle(
+          fontSize: 16,
+          color: Colors.black,
+          fontWeight: FontWeight.w400,
+        ),
+        prefixIcon: Icon(prefixIcon, color: Colors.grey),
+        suffixIcon: suffixIcon,
+        contentPadding: EdgeInsets.symmetric(
+          horizontal: 18,
+          vertical: 18,
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16),
+          borderSide: BorderSide(
+            color: Colors.black,
+            width: 1.1,
+          ),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16),
+          borderSide: BorderSide(
+            color: Colors.black,
+            width: 1.3,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// Halaman setelah login
+class NextPage extends StatelessWidget {
+  const NextPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Next Page')),
+      body: Center(
+        child: Text('Ini halaman berikutnya setelah Login.'),
+      ),
+    );
+  }
+}
+
+class _SocialAssetButton extends StatelessWidget {
+  final String assetPath;
+
+  const _SocialAssetButton({
+    required this.assetPath,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 64,
+      height: 64,
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.black, width: 1.5),
+        borderRadius: BorderRadius.circular(16),
+        color: Colors.white,
+      ),
+      child: Padding(
+        padding: EdgeInsets.all(10),
+        child: Image.asset(
+          assetPath,
+          width: 10,
+          height: 10,
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:ppkd_b7/day_10/tugas_5.dart';
+import 'package:ppkd_b7/day_11/tugas_6.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: Tugas5(),
+      home: Tugas6(),
     );
   }
 }
